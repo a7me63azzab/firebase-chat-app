f1c1e6a13058900218dceb5d2e617592c619a932
Blender2016 <devahmedazzab2014@gmail.com>
2018-02-06 03:12:08 +0200

    second commit

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 var config = {
-    apiKey: "AIzaSyC529U8G0QzMBoIHsbj0hs0j3cepzbXmBg",
-    authDomain: "one-to-one-chat-f2505.firebaseapp.com",
-    databaseURL: "https://one-to-one-chat-f2505.firebaseio.com",
-    projectId: "one-to-one-chat-f2505",
-    storageBucket: "one-to-one-chat-f2505.appspot.com",
-    messagingSenderId: "736659700018"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
